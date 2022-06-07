bb66b93a862e00735017f7d07e14dd532f1cb5bd
bip5 <bip5@aber.ac.uk>
2022-06-07 13:44:47 +0100

    eval plus csv save from vid frames

--- video_pred.py
+++ video_pred.py
@@ -1,4 +1,4 @@
-# Copyright 2020 MONAI Consortium
+ # Copyright 2020 MONAI Consortium
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -49,6 +49,18 @@ parser=argparse.ArgumentParser()
 parser.add_argument("--path", default="./videos/GH010022.MP4",type=str,help="path for video")
 parser.add_argument("--output",default="./FrameSave/",type=str,help="where to save the output")
 
+parser.add_argument("--batch_size", default=1, type=int,help="number of images to process for each step of gradient descent")
+parser.add_argument("--model",default="DenseNet121",type=str, help="DenseNet121 or DenseNet264")
+parser.add_argument("--load_save",default=0, type=int,help="load saved weights from previous training")
+parser.add_argument("--load_name",default='best_metric_modelDenseNet121auc.pth',type=str,help="name of model to be loaded")
+parser.add_argument("--epochs", default=100, type=int, help="number of epochs to run")
+parser.add_argument("--opt",default="acc",type=str, help= "Optimisation metric to use- 'auc' or 'acc'")
+parser.add_argument("--comb_ch",default=1,type=int, help="train with all 3 channels or combine them into 1")
+parser.add_argument("--eval_only",default=0,type=int,help="Eval only with loaded model")
+parser.add_argument("--eval_model",default="best_metric_model2022-03-16DenseNet121acc.pth",type=str, help="model to be loaded for eval")
+parser.add_argument("--method", default="std", type=str, help="save str to diff models while training in parallel")
+parser.add_argument("--seed", default="0", type=int, help="random seed")
+
 args=parser.parse_args()
 
 print(' '.join(sys.argv))
@@ -57,6 +69,8 @@ print(' '.join(sys.argv))
 set_determinism(seed=0)
 
 class_names = ["Eyes Open","Blinking"]
+
+num_class = len(class_names) 
 
 df=pd.read_excel("./Blink rate automation.xlsx",sheet_name=None) # reading in from excel as dict of dataframes
 #trivia: locals()["str"] = whatever to turn str into variable called str
@@ -74,124 +88,106 @@ ground_truth=np.zeros(video_length+1)
 for row in range(len(df["GH010022.MP4"])):
     # print(row)
     ground_truth[int(df["GH010022.MP4"]['Eye Closed Frame'][row]) : int(df["GH010022.MP4"]['Eye opening'][row])]=1
+
+image_files=[os.path.join('./All Frames','GH010022',x) for x in os.listdir('./All Frames/GH010022/')]
+label_path='./All Frames/GH010022/label.npy'
+image_class=np.load(label_path)
+
+test_x=image_files
+test_y=image_class
+
+val_transforms = Compose(
+    [LoadImage(image_only=True),  ScaleIntensity(), EnsureType()])
     
+y_pred_trans = Compose([EnsureType(), Activations(softmax=True)])
+y_trans = Compose([EnsureType(), AsDiscrete(to_onehot=num_class)])
 
+class MedNISTDataset(torch.utils.data.Dataset):
+    def __init__(self, image_files, labels, transforms):
+        self.image_files = image_files
+        self.labels = labels
+        self.transforms = transforms
 
+    def __len__(self):
+        return len(self.image_files)
 
-
-num_class = len(class_names) 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-model = DenseNet121(spatial_dims=2, in_channels=3,
-                    out_channels=num_class).to(device)
-model_name="best_metric_model2022-06-06DenseNet121f1s2"#"2022-05-12T22zoo_avg"
-model=torch.nn.DataParallel(model)
-model.load_state_dict(torch.load( "./"+model_name))
-
-
-vc_obj=cv2.VideoCapture(args.path)
-W,H=None,None
-
-video_length = int(vc_obj.get(cv2.CAP_PROP_FRAME_COUNT)) -1 # -1 should give exact length but sometimes some frames are missing which causes endless looping
-print("video length", video_length)
-Q=deque(maxlen=10)
-pred_label=[]
-pred_label_processed=[]
-ind=[]
-conv_ar0=[]
-conv_ar1=[]
-while vc_obj.isOpened():
-    writer=None
-    for i in range(video_length):
-        rtn_flag,frame=vc_obj.read()
-        if not rtn_flag:
-            continue
+    def __getitem__(self, index):
+        image= self.transforms(self.image_files[index])
+        if image.shape[1]==1440:
+            image=torch.movedim(image,1,0)            
+            
         
-        if W is None or H is None:
-            (H,W)=frame.shape[:2]
-        print(frame.shape)    
-        image=cv2.resize(frame,(480,360),interpolation = cv2.INTER_AREA)
-        output=image.copy()
-        #frame=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+       
+        image= torch.movedim(image,-1,0)  
+        
+       
         image1=torch.zeros((3,360,480))
         # image1=torch.tensor(cv2.resize(image2,(480,360), interpolation=cv2.INTER_AREA))
         image1[0,:,:]=torch.tensor(cv2.resize(np.array(image[0,:,:]),(480,360),interpolation=cv2.INTER_AREA))
         image1[1,:,:]=torch.tensor(cv2.resize(np.array(image[1,:,:]),(480,360),interpolation=cv2.INTER_AREA))
         image1[2,:,:]=torch.tensor(cv2.resize(np.array(image[2,:,:]),(480,360),interpolation=cv2.INTER_AREA))
+        print('image1.shape=',image1.shape)
         
-        
-        with torch.no_grad():
-            frame=image1.to(device)#torch.tensor(image).to(device)
-            #frame=frame.moveaxis(-1,0)
-            frame=frame[None,:].float()
-          
-            print(frame.shape)
-            pred=model(frame)[0].argmax(dim=1)
-            print(pred)
-            conv_ar0.append(pred[0].cpu().numpy())
+        if args.comb_ch==1:     
+            image=image[0,:,:]*0.114+image[1,:,:]*0.587+image[2,:,:]*0.299
+            image=cv2.resize(np.array(image),(360,480),interpolation=cv2.INTER_AREA)
+                
+            image=image[None,:,:]
+            print(image.shape)
+       
             
-            conv_ar1.append(pred[1].cpu().numpy())
-            np_pred=np.argmax(pred.cpu().numpy())         
-            # print(np_pred)
-            pred_label.append(np_pred)
-            Q.append(pred) # queue object only stores maxlen objects then ejects the oldest 
-            # print(np.array(Q)[0].shape)
-            npQ=np.array([j.cpu().numpy() for j in Q])
-            # print(type(npQ),npQ.shape,npQ)
-            result=npQ.mean(axis=0)
-            
-            # print(result.shape)
-             
-            blink=np.argmax(result)
-            
-         
-                        
-            label=class_names[blink]
-            
-           
-            pred_label_processed.append(blink)
-            ind.append(i)
-            
+        return image1, self.labels[index]
         
-        
-        
-        # #draw the activity on output frame
-        # text="{}".format(label)
-        # cv2.putText(output,text,(35,50),cv2.FONT_HERSHEY_SIMPLEX,1.25,(0,255,0),5)
-        
-        # cv2.imwrite(args.output+str(i)+".jpg",output)
-        
-        # if writer is None:
-        #     four_cc=cv2.VideoWriter_fourcc(*"MJPG")
-        #     writer=cv2.VideoWriter(args.output,four_cc,30,(W,H),True)
-            
-        # writer.write(output)
-        print(i,"/",video_length)
+test_ds = MedNISTDataset(test_x, test_y, val_transforms)
+test_loader = torch.utils.data.DataLoader(
+    test_ds, batch_size=1, num_workers=4)
     
-    break
-        # cv2.imshow("HorseCare",output)
-        # key=cv2.waitKey(1) & 0xFF
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+if args.model=="EfficientNetBN":
+        model = locals()[args.model]("efficientnet-b0",spatial_dims=2, in_channels=in_ch,
+                            num_classes=num_class,pretrained=False).to(device)
+else:
+    model = locals()[args.model](spatial_dims=2, in_channels=in_ch,out_channels=num_class,pretrained=False).to(device)
+model=torch.nn.DataParallel(model)  
+model.load_state_dict(torch.load('./'+args.eval_model))
+     
+model.eval()
+y_true = []
+y_pred = []
+with torch.no_grad():
+    for test_data in test_loader:
+        test_images, test_labels = (
+            test_data[0].to(device),
+            test_data[1].to(device),
+        )
         
-        # #if q pressed break from loop
-        # if key==ord("q"):
-        #     break
-    
-            
-pred_array=np.array(pred_label)
-kernel=np.ones(10)
-conv_avg0=np.convolve(np.array(conv_ar0), kernel,'same')/len(kernel)
-conv_avg1=np.convolve(np.array(conv_ar1), kernel,'same')/len(kernel)
-conv_avg=np.stack((conv_avg0,conv_avg1))
-conv_pred=np.argmax(conv_avg, axis=0)
+        pred = model(test_images).argmax(dim=1)
+        print(pred, len(test_loader)
+        
+        for i in range(len(pred)):
+            y_true.append(test_labels[i].item())
+            y_pred.append(pred[i].item())
+        
+y_pred_avg=np.convolve(np.array(y_pred),np.ones(5),'same')
+y_pred=(y_pred>0.5).astype(int)  
+y_true=np.array(y_true)
+
+print("len( y_pred)=", len( y_pred), "len(y_true)=",len(y_true))
+print("type(y_pred), type(y_true)=",type(y_pred), type(y_true))
+print('y_pred, y_true',y_pred, y_true)
+print(classification_report(
+    y_true, y_pred, target_names=class_names, digits=4))
+
 
 # release the file pointers
 df=pd.DataFrame()
-df["Human label"]=ground_truth[ind]
-df["Frame-wise Pred"]=pred_label
-df["Moving average"]=pred_label_processed
-df["Conv processed"]=conv_pred
+df["Human label"]=y_true
+df["Frame-wise Pred"]=y_pred
+df["Conv Moving average"]=y_pred_avg
 
-df.to_csv(f"./GH010022_{model_name}pred{len(Q)}.csv")
+
+df.to_csv(f"./GH010022_{args.eval_model}pred{len(Q)}.csv")
 
 print("[INFO] cleaning up...")
 # writer.release()
